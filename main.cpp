9df3c160547e227b247204611bd398649b82f7d3
Yoshiamasa Awata <yoshia.awata@nifty.ne.jp>
2025-05-15 19:40:32 +0900

    バージョン情報等の追加

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-﻿#include "mainwindow.h"
+﻿#include <QApplication>
 
-#include <QApplication>
+#include "mainwindow.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
+  a.setApplicationVersion(APP_VERSION);
   MainWindow w;
   w.show();
   return a.exec();
